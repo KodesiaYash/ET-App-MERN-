b09190a42c9ca33ce4932f8ed86e55b898ca4a8d
KodesiaYash <yash.kodesia98@gmail.com>
2020-05-04 04:32:10 +0530

    Initial commit

---
+++ et-mern/backend/server.js
@@ -0,0 +1,29 @@
+// importing modules and frameworks
+const express = require('express') ;
+// for getting cross domain data 
+const cors = require('cors') ;
+// for connecting mongoDB to nodeJS
+const mongoose = require('mongoose') ;
+// enviornment variable files 
+require('dotenv').config() ;
+
+// ---------- Connecting the database --------- 
+// Connection string from Mongo DB 
+const uri = process.env.ATLAS_URI ;
+
+// connecting MongoDB 
+mongoose.connect(uri , { useNewUrlParser : true , useCreateIndex : true } ) ;
+const connection = mongoose.connection ;
+connection.once('open' , () => {
+    console.log("MongoDB database connected successfully") ;
+})
+
+// -------------- Creating the app ------------ 
+const app = express() ;
+const port = process.env.PORT || 5000 ;
+
+app.use(cors()) ;
+app.use(express.json()) ;
+
+app.listen(port,() => 
+    {console.log(`Server is running on port : ${port}`)}) ;
